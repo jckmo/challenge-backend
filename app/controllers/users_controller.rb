cf319dddee6776268e9b469aa5eabc7fa86a4c05
Jack <jacksonmorgan@hey.com>
2021-05-06 01:41:32 -0400

    working with user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.find_by(username: params[:userName])
+
+    if !@user
+      @user = User.create(username: params[:userName], password_digest: params[:password], is_master?: false)
+      render json: @user.id
+    else
+      @message = 'This user already exists, please login or choose a different username'
+
+      render json: @messsage
+    end
+  end
 end
