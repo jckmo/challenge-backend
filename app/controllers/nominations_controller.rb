08704d8033a312806231cf935a6611ddfe7c4ccb
Jack <jacksonmorgan@hey.com>
2021-05-07 11:56:50 -0400

    first mvp test

--- app/controllers/nominations_controller.rb
+++ app/controllers/nominations_controller.rb
@@ -1,8 +1,8 @@
 class NominationsController < ApplicationController
   def create
-
     if params[:source] == 'fromSearch'
       @title = Title.find_by(title: params[:title][:Title])
+
       if @title
         @times_nominated = Nomination.select{|ind| ind.title_id == @title.id}.count
         @title_to_fit_state = {title: @title, times_nominated: @times_nominated}
@@ -49,11 +49,20 @@ class NominationsController < ApplicationController
   end
 
   def destroy
-    # need to add source control
     if params[:source] == 'fromSearch'
-      @nomination = Nomination.find_by(title_id: Title.find_by(title: params[:title][:Title])).destroy
+      Nomination.find_by(title_id: Title.find_by(title: params[:title][:Title])).destroy
+      @title = Title.find_by(title: params[:title][:Title])
+      @ind_times_nominated = Nomination.select{|ind| ind.title_id == @title.id}.count
+      if @ind_times_nominated < 1
+        Title.find_by(title: params[:title][:Title]).destroy
+      end
     elsif params[:source] == 'fromNoms'
-      @nomination = Nomination.find_by(title_id: Title.find_by(title: params[:title][:title])).destroy
+      Nomination.find_by(title_id: Title.find_by(title: params[:title][:title])).destroy
+      @title = Title.find_by(title: params[:title][:title])
+      @ind_times_nominated = Nomination.select{|ind| ind.title_id == @title.id}.count
+      if @ind_times_nominated < 1
+        Title.find_by(title: params[:title][:title]).destroy
+      end
     end
 
     @nominated_titles = []
